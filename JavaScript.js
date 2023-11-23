5ae9787f1d03282bcda7db0eddc0da5421a4df7b
Gvijayasurya <113319874+Gvijayasurya@users.noreply.github.com>
2023-11-23 23:26:48 +0530

    Added Html, CSS, JS files.

---
+++ JavaScript.js
@@ -0,0 +1,60 @@
+function AC(){
+    let b = document.getElementsByTagName('p')[0].innerText = ''
+}
+function del(){
+    let b = document.getElementsByTagName('p')[0].innerText = document.getElementsByTagName('p')[0].innerText.slice(0,-1)
+}
+function per(){
+    let b = document.getElementsByTagName('p')[0].innerText += '%'
+}
+function div(){
+    let b = document.getElementsByTagName('p')[0].innerText += '/'
+}
+function seven(){
+    let b = document.getElementsByTagName('p')[0].innerText += '7'
+}
+function eight(){
+    let b = document.getElementsByTagName('p')[0].innerText += '8'
+}
+function nine(){
+    let b = document.getElementsByTagName('p')[0].innerText += '9'
+}
+function mul(){
+    let b = document.getElementsByTagName('p')[0].innerText += '*'
+}
+function four(){
+    let b = document.getElementsByTagName('p')[0].innerText += '4'
+}
+function five(){
+    let b = document.getElementsByTagName('p')[0].innerText += '5'
+}
+function six(){
+    let b = document.getElementsByTagName('p')[0].innerText += '6'
+}
+function sub(){
+    let b = document.getElementsByTagName('p')[0].innerText += '-'
+}
+function one(){
+    let b = document.getElementsByTagName('p')[0].innerText += '1'
+}
+function two(){
+    let b = document.getElementsByTagName('p')[0].innerText += '2'
+}
+function three(){
+    let b = document.getElementsByTagName('p')[0].innerText += '3'
+}
+function sum(){
+    let b = document.getElementsByTagName('p')[0].innerText += '+'
+}
+function dzero(){
+    let b = document.getElementsByTagName('p')[0].innerText += '00'
+}
+function zero(){
+    let b = document.getElementsByTagName('p')[0].innerText += '0'
+}
+function point(){
+    let b = document.getElementsByTagName('p')[0].innerText += '.'
+}
+function equal(){
+    let b = document.getElementsByTagName('p')[0].innerText = eval(document.getElementsByTagName('p')[0].innerText)
+}
